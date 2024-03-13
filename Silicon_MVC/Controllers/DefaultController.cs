4136e293b3e16a61cc0cffae30140cfdbec7a037
Gala <70331948+galinamiteva@users.noreply.github.com>
2024-03-13 21:32:15 +0100

    Init

---
+++ Silicon_MVC/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_MVC.Controllers;
+
+public class DefaultController : Controller
+{
+    [Route("/")]
+    public IActionResult Home() => View();
+
+
+
+    [Route("/error")]
+    public IActionResult Error404(int statusCode) => View();
+
+
+    [Route("/dehied")]
+    public IActionResult AccessDenied(int statusCode) => View();
+}
